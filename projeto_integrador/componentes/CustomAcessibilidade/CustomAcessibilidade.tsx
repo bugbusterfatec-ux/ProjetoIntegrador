b068612e296cd604210c4e5605725b51d89f6dee
Tiago <bugbuster.fatec@gmail.com>
2026-04-30 22:11:59 -0300

    alterado as classes

--- projeto_integrador/componentes/CustomAcessibilidade/CustomAcessibilidade.tsx
+++ projeto_integrador/componentes/CustomAcessibilidade/CustomAcessibilidade.tsx
@@ -1,25 +1,26 @@
 'use client'
+import style from "./CustomAcessibilidade.module.css"
 
 
 export const CustomAcessibilidade = () => {
     
     return (
-        <div className="barra-acessibilidade bg-light" >
+        <div className={`${style["barra-acessibilidade"]} bg-ligth`} >
             <div className="container-fluid">
                 <div className="d-flex justify-content-start align-items-center gap-2 py-2">
-                    <button className="btn-acessibilidade btn-contraste" aria-label="Contraste">
+                    <button className={`${style["btn-acessibilidade"]} btn-contraste`} aria-label="Contraste">
                         <span className="material-symbols-rounded">fiber_manual_record</span>
                     </button>
 
-                    <button className="btn-acessibilidade btn-vermelho" id="aumentarFonte">
+                    <button className={`${style["btn-acessibilidade"]} btn-vermelho`} id="aumentarFonte">
                         A+
                     </button>
 
-                    <button className="btn-acessibilidade btn-vermelho" id="diminuirFonte">
+                    <button className={`${style["btn-acessibilidade"]} btn-vermelho`} id="diminuirFonte">
                         A-
                     </button>
 
-                    <button className="btn-acessibilidade" aria-label="Modo audiodescrição">
+                    <button className={style["btn-acessibilidade"]} aria-label="Modo audiodescrição">
                         <span className="material-symbols-rounded">hearing</span>
                     </button>
 
