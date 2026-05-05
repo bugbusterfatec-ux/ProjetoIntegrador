0dc37a16ff8c2d8f00900d65803a5371ea1d2c95
Tiago <bugbuster.fatec@gmail.com>
2026-05-05 17:03:01 -0300

    Implementado a page meetinPet

--- projeto_integrador/componentes/CustomSection/CustomSection.tsx
+++ projeto_integrador/componentes/CustomSection/CustomSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./CustomSection.module.css"
 
 interface CustomSectionProps {
-    children : React.ReactNode[];
+    children : React.ReactNode;
     className? : string;
 }
 
@@ -10,7 +10,7 @@ export const CustomSection = (props : CustomSectionProps) => {
 
     return (
         <section className={style["filtro-container"]}>
-            {...props.children}
+            {props.children}
         </section>
     )
 }
