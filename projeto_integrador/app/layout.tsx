69428f95ee6f4a10e4b7581fdc0f23a82f08042b
Ariana Vieira <ariana.av191@gmail.com>
2026-05-20 22:19:31 -0300

    tentativa de exibir o vlibras

--- projeto_integrador/app/layout.tsx
+++ projeto_integrador/app/layout.tsx
@@ -1,5 +1,3 @@
-//import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./globals.css";
 import { VLibrasWidget } from "@/componentes/VLibrasWidget/VLibrasWidget";
