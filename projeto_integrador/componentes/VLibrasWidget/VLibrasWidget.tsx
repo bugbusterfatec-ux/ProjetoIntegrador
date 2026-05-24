fbbd59c093a97eba2967839be99731554aecdd05
Ariana Vieira <ariana.av191@gmail.com>
2026-05-24 16:12:35 -0300

    Adicionar atributos customizados via useEffect após renderização
    - Remover atributos customizados do HTML para evitar problemas de hidratação
    - Adicionar vw, vw-access-button via setAttribute() no useEffect
    - Garantir que atributos sejam aplicados depois que React renderizar
    - VLibras pode agora encontrar corretamente os elementos

--- projeto_integrador/componentes/VLibrasWidget/VLibrasWidget.tsx
+++ projeto_integrador/componentes/VLibrasWidget/VLibrasWidget.tsx
@@ -1,8 +1,21 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const VLibrasWidget = () => {
+    const wrapperRef = useRef<HTMLDivElement>(null)
+
     useEffect(() => {
+        // Adiciona os atributos customizados após o React renderizar
+        if (wrapperRef.current) {
+            const mainDiv = wrapperRef.current.querySelector('div')
+            if (mainDiv) {
+                mainDiv.setAttribute('vw', '')
+                const children = mainDiv.querySelectorAll('div')
+                if (children[0]) children[0].setAttribute('vw-access-button', '')
+                if (children[1]) children[1].setAttribute('vw-plugin-wrapper', '')
+            }
+        }
+
         // Carrega o script do VLibras
         const script = document.createElement('script')
         script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js'
@@ -42,9 +55,10 @@ export const VLibrasWidget = () => {
 
     return (
         <div
+            ref={wrapperRef}
             className="vlibras-wrapper"
             dangerouslySetInnerHTML={{
-                __html: '<div vw class="enabled"><div vw-access-button class="active"></div><div vw-plugin-wrapper><div class="vw-plugin-top-wrapper"></div></div></div>'
+                __html: '<div class="enabled"><div class="active"></div><div><div class="vw-plugin-top-wrapper"></div></div></div>'
             }}
         />
     )
